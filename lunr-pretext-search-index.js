6abfbdb0bdddc9cbba9d459156e72e021e0f2d7e
DeMoulpied <jwdemoul@colostate.edu>
2024-11-05 21:59:29 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,356 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "Chapter1Section1",
+  "level": "1",
+  "url": "Chapter1Section1.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Fixed and Varying Quantities",
+  "body": " Fixed and Varying Quantities  Text of section.  "
+},
+{
+  "id": "Chapter1Section2",
+  "level": "1",
+  "url": "Chapter1Section2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Functions and Notation",
+  "body": " Functions and Notation  Text of section.  "
+},
+{
+  "id": "Chapter1Section3",
+  "level": "1",
+  "url": "Chapter1Section3.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Domain and Range",
+  "body": " Domain and Range  Text of section.  "
+},
+{
+  "id": "Chapter1Section4",
+  "level": "1",
+  "url": "Chapter1Section4.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Rates of Change",
+  "body": " Rates of Change  Text of section.  "
+},
+{
+  "id": "Chapter1Section5",
+  "level": "1",
+  "url": "Chapter1Section5.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Composition",
+  "body": " Composition  Text of section.  "
+},
+{
+  "id": "Chapter1Section6",
+  "level": "1",
+  "url": "Chapter1Section6.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Transformations",
+  "body": " Transformations  Text of section.  "
+},
+{
+  "id": "Chapter1Section7",
+  "level": "1",
+  "url": "Chapter1Section7.html",
+  "type": "Section",
+  "number": "1.7",
+  "title": "Inverse Functions",
+  "body": " Inverse Functions  Text of section.  "
+},
+{
+  "id": "Chapter2Section1",
+  "level": "1",
+  "url": "Chapter2Section1.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Linear Functions",
+  "body": " Linear Functions  Text of section.  "
+},
+{
+  "id": "Chapter2Section2",
+  "level": "1",
+  "url": "Chapter2Section2.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Graphs of Linear Functions",
+  "body": " Graphs of Linear Functions  Text of section.  "
+},
+{
+  "id": "Chapter2Section3",
+  "level": "1",
+  "url": "Chapter2Section3.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Modeling with Linear Functions",
+  "body": " Modeling with Linear Functions  Text of section.  "
+},
+{
+  "id": "Chapter2Section4",
+  "level": "1",
+  "url": "Chapter2Section4.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Absolute Value Functions",
+  "body": " Absolute Value Functions  Text of section.  "
+},
+{
+  "id": "Chapter3Section1",
+  "level": "1",
+  "url": "Chapter3Section1.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Quadratic Functions",
+  "body": " Quadratic Functions  Text of section.  "
+},
+{
+  "id": "Chapter3Section2",
+  "level": "1",
+  "url": "Chapter3Section2.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Polynomial Functions",
+  "body": " Polynomial Functions  Text of section.  "
+},
+{
+  "id": "Chapter3Section3",
+  "level": "1",
+  "url": "Chapter3Section3.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Graphs of Polynomial Functions",
+  "body": " Graphs of Polynomial Functions  Text of section.  "
+},
+{
+  "id": "Chapter3Section4",
+  "level": "1",
+  "url": "Chapter3Section4.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Roots of Polynomials",
+  "body": " Roots of Polynomials  Text of section.  "
+},
+{
+  "id": "Chapter3Section5",
+  "level": "1",
+  "url": "Chapter3Section5.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Rational Functions and End Behavior",
+  "body": " Rational Functions and End Behavior  Text of section.  "
+},
+{
+  "id": "Chapter3Section6",
+  "level": "1",
+  "url": "Chapter3Section6.html",
+  "type": "Section",
+  "number": "3.6",
+  "title": "Rational Functions and Vertical Asymptotes",
+  "body": " Rational Functions and Vertical Asymptotes  Text of section.  "
+},
+{
+  "id": "Chapter3Section7",
+  "level": "1",
+  "url": "Chapter3Section7.html",
+  "type": "Section",
+  "number": "3.7",
+  "title": "Graphing Rational Functions",
+  "body": " Graphing Rational Functions  Text of section.  "
+},
+{
+  "id": "Chapter4Section1",
+  "level": "1",
+  "url": "Chapter4Section1.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Percentages and Percent Change",
+  "body": " Percentages and Percent Change  Text of section.  "
+},
+{
+  "id": "Chapter4Section2",
+  "level": "1",
+  "url": "Chapter4Section2.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Exponential Functions",
+  "body": " Exponential Functions  Text of section.  "
+},
+{
+  "id": "Chapter4Section3",
+  "level": "1",
+  "url": "Chapter4Section3.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Exponential Growth and Decay Factors",
+  "body": " Exponential Growth and Decay Factors  Text of section.  "
+},
+{
+  "id": "Chapter4Section4",
+  "level": "1",
+  "url": "Chapter4Section4.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Exponential Models",
+  "body": " Exponential Models  Text of section.  "
+},
+{
+  "id": "Chapter4Section5",
+  "level": "1",
+  "url": "Chapter4Section5.html",
+  "type": "Section",
+  "number": "4.5",
+  "title": "Logarithmic Functions",
+  "body": " Logarithmic Functions  Text of section.  "
+},
+{
+  "id": "Chapter4Section6",
+  "level": "1",
+  "url": "Chapter4Section6.html",
+  "type": "Section",
+  "number": "4.6",
+  "title": "Logarithmic Properties",
+  "body": " Logarithmic Properties  Text of section.  "
+},
+{
+  "id": "Chapter4Section7",
+  "level": "1",
+  "url": "Chapter4Section7.html",
+  "type": "Section",
+  "number": "4.7",
+  "title": "Solving Exponential and Logarithmic Equations",
+  "body": " Solving Exponential and Logarithmic Equations  Text of section.  "
+},
+{
+  "id": "Chapter5Section1",
+  "level": "1",
+  "url": "Chapter5Section1.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Circles and Angles",
+  "body": " Circles and Angles  Text of section.  "
+},
+{
+  "id": "Chapter5Section2",
+  "level": "1",
+  "url": "Chapter5Section2.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Representing Circular Motion",
+  "body": " Representing Circular Motion  Text of section.  "
+},
+{
+  "id": "Chapter5Section3",
+  "level": "1",
+  "url": "Chapter5Section3.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "The Tangent Function",
+  "body": " The Tangent Function  Text of section.  "
+},
+{
+  "id": "Chapter5Section4",
+  "level": "1",
+  "url": "Chapter5Section4.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Right Triangle Trigonometry",
+  "body": " Right Triangle Trigonometry  Text of section.  "
+},
+{
+  "id": "Chapter6Section1",
+  "level": "1",
+  "url": "Chapter6Section1.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Periodic Graphs",
+  "body": " Periodic Graphs  Text of section.  "
+},
+{
+  "id": "Chapter6Section2",
+  "level": "1",
+  "url": "Chapter6Section2.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Period and Amplitude",
+  "body": " Period and Amplitude  Text of section.  "
+},
+{
+  "id": "Chapter6Section3",
+  "level": "1",
+  "url": "Chapter6Section3.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Transformations of Periodic Functions",
+  "body": " Transformations of Periodic Functions  Text of section.  "
+},
+{
+  "id": "Chapter6Section4",
+  "level": "1",
+  "url": "Chapter6Section4.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Inverse Trig Functions",
+  "body": " Inverse Trig Functions  Text of section.  "
+},
+{
+  "id": "Chapter6Section5",
+  "level": "1",
+  "url": "Chapter6Section5.html",
+  "type": "Section",
+  "number": "6.5",
+  "title": "Solving Trig Equations",
+  "body": " Solving Trig Equations  Text of section.  "
+},
+{
+  "id": "Chapter6Section6",
+  "level": "1",
+  "url": "Chapter6Section6.html",
+  "type": "Section",
+  "number": "6.6",
+  "title": "Modeling with Trigonometric Equations",
+  "body": " Modeling with Trigonometric Equations  Text of section.  "
+},
+{
+  "id": "Chapter6Section7",
+  "level": "1",
+  "url": "Chapter6Section7.html",
+  "type": "Section",
+  "number": "6.7",
+  "title": "Law of Sines and Cosines",
+  "body": " Law of Sines and Cosines  Text of section.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
